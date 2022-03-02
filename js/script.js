fc0589076a31a81456e943949b57dccd52db6bce
Bekmirzo <99716618+Bekmirzo@users.noreply.github.com>
2022-03-02 09:13:56 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
+var t = document.getElementsByClassName('it')[0];
+var qm = document.getElementsByClassName('qm')[0];
+var style = window.getComputedStyle(t);
 
+qm.addEventListener('click',()=>{
+    if(style.height == '0px'){
+        t.style.height = `${t.scrollHeight}px`;
+        t.style.transition = 'height 0.25s ease-in';
+    } else {
+        t.style.height = '0px';
+    }
+})
